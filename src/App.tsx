4f01e8bc4ea713bc4d4720031f57db38f797350a
Alexandre Consigny <alexandre.consigny@gmail.com>
2023-03-10 13:56:15 -0500

    feat: send local messages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
+
+type Message = string;
+
 const App = () => {
+  const [currentMessage, setCurrentMessage] = useState<Message>('');
+  const [chat, addMessage] = useState<Message[]>([]);
+
+  const sendMessage = (message: Message) => {
+    addMessage([...chat, message]);
+    setCurrentMessage('');
+  }
+
   return (
     <div>
       <h1>Local communicator</h1>
+      <div>
+        <ul>
+          {chat.map((message, index) => {
+            return (
+              <li key={index}>{message}</li>
+            )
+          })}
+        </ul>
+      </div>
+      <div>
+        <input value={currentMessage} onChange={(event) => setCurrentMessage(event.target.value)} />
+        <button onClick={() => sendMessage(currentMessage)} >Send!</button>
+      </div>
     </div>
   )
 }
