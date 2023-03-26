4f268e0d56f6d32e2cabec742e47dbc4ca082451
Alexandre Consigny <alexandre.consigny@gmail.com>
2023-03-26 18:57:44 -0400

    revert it

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,7 +2,12 @@ importScripts('https://www.gstatic.com/firebasejs/9.17.0/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/9.17.0/firebase-messaging-compat.js');
 
 const firebaseConfig = {
+  apiKey: "AIzaSyC6ZNSwd75_PfHXRtdI8a_TD4B7GBwA_V0",
+  authDomain: "chat-rooms-40341.firebaseapp.com",
+  projectId: "chat-rooms-40341",
+  storageBucket: "chat-rooms-40341.appspot.com",
   messagingSenderId: "890323753860",
+  appId: "1:890323753860:web:30213a4f418e3e6c6a1bd1"
 };
 
 const app = firebase.initializeApp(firebaseConfig);
