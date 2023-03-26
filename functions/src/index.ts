7588cb67fd009cb280fd6c5b0d1d7ec4b42f5a06
Alexandre Consigny <alexandre.consigny@gmail.com>
2023-03-26 15:02:08 -0400

    chore: update functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,35 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
 
-// // Start writing Firebase Functions
+// const app = initializeApp();
+
+// // These registration tokens come from the client FCM SDKs.
+// const registrationTokens = [
+//   'YOUR_REGISTRATION_TOKEN_1',
+//   // ...
+//   'YOUR_REGISTRATION_TOKEN_n'
+// ];
+
+// // Subscribe the devices corresponding to the registration tokens to the
+// // topic.
+// getMessaging().subscribeToTopic(registrationTokens, topic)
+//   .then((response) => {
+//     // See the MessagingTopicManagementResponse reference documentation
+//     // for the contents of response.
+//     console.log('Successfully subscribed to topic:', response);
+//   })
+//   .catch((error) => {
+//     console.log('Error subscribing to topic:', error);
+//   });
+
+// // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
-//
+
+// const registerWebNotifications = functions.region('ca-central1').https.onRequest((request, response) => {
+
+// });
+
+// exports.registerWebNotifications = registerWebNotifications;
+
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
