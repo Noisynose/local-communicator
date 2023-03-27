c3f8de1a7837f07f5a48263dbc84d9236ecf06e5
Vero Lopez <vero@mysmartjourney.com>
2023-03-26 23:55:58 -0400

    favicons + config vitePWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { resolve } from 'path';
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({ registerType: "autoUpdate",
+      devOptions: {
+        enabled: true
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src')
